2e753bbb9808aa21064e6058b227bad0794ac6ea
AKASH RAJ <akashraj9828@users.noreply.github.com>
2017-08-23 08:38:53 +0530

    commit

--- tile.js
+++ tile.js
@@ -2,18 +2,37 @@ function tile() {
 
     this.xvel=50
     this.x = 0
+    this.prevx=0;
     this.y = 0
+    this.prevy=0;
     this.gravity=5
 
 
-    this.draw = function () {
+    this.draw = function (dir) {
+        del=this.prevy-this.y
+        console.log(del)
         fill(0)
         push()
         rotate(PI / 4);
         rectMode(CENTER);
         pop()
         rect(this.x, this.y, 100, 100)
+        this.prevx=this.x
+        this.prevy=this.y
         this.y+=this.gravity;
+
+        if(dir==-1){
+            this.xvel=1
+            this.x+=-this.xvel;
+            this.y-=100
+            if(this.xvel<50){
+            this.xvel++
+            }
+            else do{
+                this.xvel--
+            }while (this.xvel>0)
+            this.moveL();
+        }
     }
 
     // this.update = function (key) {
@@ -23,10 +42,23 @@ function tile() {
     //     }
     // }
 
+    this.moveL=function(){
+        del=this.prevx-this.x
+        if(abs(del)==0){
+            console.log("xvel=0:::"+del);
+        }
+        else{
+            this.x+=-1
+        }
+        }
+    
+
     this.setvel=function(key){
         if(key==-1){
-            this.x+=-this.xvel;
-            this.y-=100
+            // this.x+=-this.xvel;
+            // this.y-=100
+            // this.moveL();
+            draw(-1);
             
         }
         else if(key==1){
@@ -34,11 +66,4 @@ function tile() {
             this.y-=100
         }
     }
-
-
-
-
-
-
-
 }
